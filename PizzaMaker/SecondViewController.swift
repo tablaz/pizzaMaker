1054a653934d7a9e38b0ab7194d8faabf867aea8
Ricardo Restrepo M <rrestrepo@seeketing.com>
2016-03-06 18:04:53 +0000

    Initial Commit

---
+++ PizzaMaker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PizzaMaker
+//
+//  Created by Ricardo on 06/03/2016.
+//  Copyright © 2016 Tablaz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
