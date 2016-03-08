31a3783783a46974146f4913c33f1e43b8c91fd0
Ricardo Restrepo M <rrestrepo@seeketing.com>
2016-03-08 18:17:01 +0000

    Cambio de Splash

--- PizzaMaker/HornoVC.swift
+++ PizzaMaker/HornoVC.swift
@@ -11,6 +11,10 @@ import UIKit
 class HornoVC: UIViewController {
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 
+
+    @IBOutlet weak var hornearBtn: UIButton!
+    
+
     @IBOutlet weak var tamanoLabel: UILabel!
     @IBOutlet weak var moreImage: UIImageView!
     @IBOutlet weak var moreCall: UILabel!
@@ -19,13 +23,14 @@ class HornoVC: UIViewController {
     @IBOutlet weak var masaLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            self.hornearBtn.hidden = true
         // Do any additional setup after loading the view.
         
         
     }
 
     override func viewWillAppear(animated: Bool) {
+        self.hornearBtn.hidden = true
         moreImage.hidden = true
         moreCall.hidden = true
         self.tamanoLabel.text = appDelegate.nuevapizza.tamano.rawValue
@@ -39,6 +44,7 @@ class HornoVC: UIViewController {
              ingredientes += "\(ing.rawValue)\n "
             }
             self.otrosLabel.text = ingredientes
+            self.hornearBtn.hidden = false
   
         } else {
             moreImage.hidden = false
@@ -48,6 +54,11 @@ class HornoVC: UIViewController {
         
         
         
+    }
+    @IBAction func hornearBtn(sender: UIButton) {
+        let alerta = UIAlertController(title: "Pizza Lista", message: "Ya puedes recoger tu Pizza", preferredStyle: UIAlertControllerStyle.Alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Cancel, handler: nil))
+        self.presentViewController(alerta, animated: true, completion: nil)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
